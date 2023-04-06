9ab6a04ef7a5796b72db9b81d36b66595ce1e8f7
finch1 <rob.caruana15@gmail.com>
2023-04-06 14:04:25 +0200

    advanced async

---
+++ HowTo/Advanced/AdvancedAsync/Program.cs
@@ -0,0 +1,173 @@
+﻿using System.Net;
+using System.Text;
+using System.Threading;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        CancellationTokenSource cts = new CancellationTokenSource();
+        
+
+        var watch = System.Diagnostics.Stopwatch.StartNew();
+        
+        for (int i = 0; i < 1; i++)
+        {
+            watch.Start();
+            System.Console.WriteLine("StartingSync");
+            PrintResults(DemoMethods.RunDownloadSync());
+            watch.Stop();
+            System.Console.WriteLine($"Elapsed Time {watch.ElapsedMilliseconds}");
+
+            Progress<ProgressReportModel> progress = new Progress<ProgressReportModel>();
+            progress.ProgressChanged += ReportProgress; // event we hooked into.
+
+            watch.Restart(); // Stops time interval measurement, resets the elapsed time to zero, and starts measuring elapsed time.
+            System.Console.WriteLine("StartingAsync");
+            PrintResults(await DemoMethods.RunDownloadAsync(progress, cts.Token)); // if we dont await, the code will exit before finishing
+            watch.Stop();
+            System.Console.WriteLine($"Elapsed Time {watch.ElapsedMilliseconds}");
+
+            watch.Restart();
+            System.Console.WriteLine("StartingParallelAsync");
+            PrintResults(await DemoMethods.RunDownloadParallelAsync());
+            watch.Stop();
+            System.Console.WriteLine($"Elapsed Time {watch.ElapsedMilliseconds}");
+        }
+        
+        System.Console.WriteLine("Done");
+    }
+
+    static void ReportProgress(object sender, ProgressReportModel e)
+    {
+        System.Console.WriteLine(e.Percentagecomplete);
+        PrintResults(e.SitesDownloadedSoFar);
+    }
+    static void PrintResults(List<WebsiteDataModel> data)
+    {
+        foreach(WebsiteDataModel result in data)
+        {
+            System.Console.WriteLine($"Method {result.MethodType}. -  {result.WebsiteUrl} downloaded {result.WebsiteData.Length} characters long.");
+        }
+    }
+
+    public static class DemoMethods
+    {
+        public static List<string> PrepData()
+        {
+            return new List<string>{
+                "https://www.facebook.com",
+                "https://www.youtube.com",
+                "https://en.wikipedia.org",
+                "https://edition.cnn.com/",
+                "https://www.microsoft.com/en-mt",
+                "https://www.office.com/",
+                "https://www.instagram.com",
+            };
+        }
+
+        public static List<WebsiteDataModel> RunDownloadSync()
+        {
+            List<string> websites = PrepData();
+            List<WebsiteDataModel> output = new List<WebsiteDataModel>();
+
+            foreach (string website in websites)
+            {            
+                WebsiteDataModel results = DownloadWebsite(website);
+                output.Add(results);
+            }
+
+            return output;
+        }    
+
+        public static async Task<List<WebsiteDataModel>> RunDownloadAsync(IProgress<ProgressReportModel> progress, CancellationTokenSource cancellationToken) // IProgress is a Generic Type. It is a callback to show progress to the user when it happenes.
+        {
+            List<string> websites = PrepData();
+            List<WebsiteDataModel> output = new List<WebsiteDataModel>();
+            ProgressReportModel report = new ProgressReportModel();
+
+
+            foreach (string website in websites)
+            {            
+                WebsiteDataModel results = await DownloadWebsiteAsync(website, "Async");
+                cancellationToken.ThrowIfCancellationRequested();                
+                output.Add(results);
+
+                report.SitesDownloadedSoFar = output;
+                report.Percentagecomplete = (output.Count()*100)/websites.Count(); // output * 100 is a trick not to have decimal
+                progress.Report(report);
+            }
+
+            return output;
+        }    
+
+        public static async Task<List<WebsiteDataModel>> RunDownloadParallelAsync()
+        {
+            List<string> websites = PrepData();
+            List<Task<WebsiteDataModel>> tasks = new List<Task<WebsiteDataModel>>();
+            
+            foreach (string website in websites)
+            {            
+                tasks.Add(DownloadWebsiteAsync(website, "ParallelAsync"));                
+            }
+
+            var results = await Task.WhenAll(tasks);
+
+            return new List<WebsiteDataModel>(results);
+        }    
+
+        public static WebsiteDataModel DownloadWebsite(string websiteURL)
+        {
+            if (websiteURL == null || websiteURL.Length == 0)
+            {
+                throw new ApplicationException("Specify the URI of the resource to retrieve.");
+            }
+            using(WebClient client = new WebClient())
+            {
+                client.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
+
+                WebsiteDataModel output = new WebsiteDataModel();
+
+                output.WebsiteUrl = websiteURL;
+                output.MethodType = "Sync";
+                output.WebsiteData = client.DownloadString(websiteURL); // index HTML
+
+                return output;
+            }        
+        }
+
+        public static async Task<WebsiteDataModel> DownloadWebsiteAsync(string websiteURL, string methodType)
+        {
+            if (websiteURL == null || websiteURL.Length == 0)
+            {
+                throw new ApplicationException("Specify the URI of the resource to retrieve.");
+            }
+            using(WebClient client = new WebClient())
+            {
+                client.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
+
+                WebsiteDataModel output = new WebsiteDataModel();
+
+                output.WebsiteUrl = websiteURL;
+                output.MethodType = methodType;
+                output.WebsiteData = await client.DownloadStringTaskAsync(new Uri(websiteURL)); // index HTML
+
+                return output;
+            }        
+        }
+
+    }
+    
+}
+public class WebsiteDataModel
+{
+    public string? WebsiteUrl { get; set; }
+    public string? WebsiteData { get; set; }
+    public string? MethodType { get; set; }
+}
+
+public class ProgressReportModel
+{
+    public List<WebsiteDataModel> SitesDownloadedSoFar {get; set;} = new List<WebsiteDataModel>();
+    public int Percentagecomplete { get; set; } = 0;
+}
--- HowTo/Advanced/Async/Program.cs
+++ HowTo/Advanced/Async/Program.cs
@@ -1,20 +1,40 @@
 ﻿using System.Net;
-
+using System.Text;
+using System.Text.RegularExpressions;
 class Program
 {
-    static void Main(string[] args)
+    static async Task Main(string[] args)
     {
-        for (int i = 0; i < 5; i++)
+        for (int i = 0; i < 3; i++)
         {
-            //ExecuteSyncClick();    
+            ExecuteSyncClick();    
         }
         
-        for (int i = 0; i < 5; i++)
+        for (int i = 0; i < 3; i++)
         {
-            ExecuteAsyncClick();    
+            // if we dont await, the code will exit before finishing
+            await ExecuteAsyncClick();    
         }
 
-        System.Console.ReadLine();        
+        // string[] downloadedSites = File.ReadAllLines("resultsParallel.txt");
+        string[] downloadedSites = File.ReadAllLines("resultsAsync.txt");
+
+        Regex match = new Regex("^http.*?.(com|org)");
+        List<string> downloadedSitesClean = downloadedSites.Select(x => match.Match(x).Value).ToList();
+
+        // since we are outputing and "annonymous class" the returned result is of type IGrouping<int, class>, where each group is an array
+        // keeps all the data
+        var dataResults = downloadedSitesClean.GroupBy(x => x).Select(x => new {Website = x, WebsiteCount = x.Count()}).Distinct();
+
+        // keeps only one copy of the data key. We loose the anonymous class though
+        var dataResults2 = downloadedSitesClean.GroupBy(x => x).ToDictionary(g => g.Key, g => g.Count());
+
+        foreach (var data in dataResults2)
+        {
+            System.Console.WriteLine($"{data.Key} \t: {data.Value}");
+        }
+
+        // System.Console.WriteLine();  
     }
     private static List<string> PrepData()
     {
@@ -28,6 +48,25 @@ class Program
             "https://www.instagram.com",
         };
     }
+    private static WebsiteDataModel DownloadWebsite(string websiteURL)
+    {
+        if (websiteURL == null || websiteURL.Length == 0)
+        {
+            throw new ApplicationException("Specify the URI of the resource to retrieve.");
+        }
+        using(WebClient client = new WebClient())
+        {
+            client.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
+
+            WebsiteDataModel output = new WebsiteDataModel();
+
+            output.WebsiteUrl = websiteURL;
+            output.WebsiteData = client.DownloadString(websiteURL); // index HTML
+
+            return output;
+        }        
+    }
+
 
 
     private static void ExecuteSyncClick()
@@ -48,39 +87,28 @@ class Program
         foreach (string website in websites)
         {            
             WebsiteDataModel results = DownloadWebsite(website);
-            ReportWebsiteInfoSync(results);
+            ReportWebsiteInfoSync(results, "resultsSync.txt");
         }
     }    
-    private static WebsiteDataModel DownloadWebsite(string websiteURL)
+    private static void ReportWebsiteInfoSync(WebsiteDataModel data, string filePath)
     {
-        if (websiteURL == null || websiteURL.Length == 0)
+        if(data is not null)
         {
-            throw new ApplicationException("Specify the URI of the resource to retrieve.");
+            string text = $"{data.WebsiteUrl} downloaded {data.WebsiteData.Length} characters long.\n";
+            File.AppendAllText(filePath, text);
         }
-        using(WebClient client = new WebClient())
-        {
-            client.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
-
-            WebsiteDataModel output = new WebsiteDataModel();
-
-            output.WebsiteUrl = websiteURL;
-            output.WebsiteData = client.DownloadString(websiteURL); // index HTML
-
-            return output;
-        }        
-    }
-    private static void ReportWebsiteInfoSync(WebsiteDataModel data)
-    {
-
-        string text = $"{data.WebsiteUrl} downloaded {data.WebsiteData.Length} characters long.";
-        System.Console.WriteLine(text);
+        
     }
 
-    private static async void ExecuteAsyncClick()
+
+
+    private static async Task ExecuteAsyncClick()
     {
         var watch = System.Diagnostics.Stopwatch.StartNew();
 
+        //await RunDownloadAsync();
         await RunDownloadAsyncParallel();
+        
 
         watch.Stop();
         var elapsedTimeMs = watch.ElapsedMilliseconds;
@@ -90,6 +118,7 @@ class Program
     // NEVER return void from an ASYNC method. We can return Task or Task<string> = when we have something to return for example. 
     private static async Task RunDownloadAsync()
     {
+        // NOT ALL RESULTS ARE SAVED TO FILE
         List<string> websites = PrepData();
 
         foreach (string website in websites)
@@ -97,14 +126,28 @@ class Program
             // Task executes our custom methods async 
             // await = run this asyncro but wait for it to finish/return. It allows a bit of flexibility for other bits to execute
             WebsiteDataModel results = await Task.Run(() => DownloadWebsite(website)); // => Pass in this code to Task to run.
-            ReportWebsiteInfoAsync(results);
+            await ReportWebsiteInfoAsync(results);
         }
     }
+    private static async Task ReportWebsiteInfoAsync(WebsiteDataModel data)
+    {
+        if(data is not null)
+        {
+            string filePath = "resultsAsync.txt";
+            string text = $"{data.WebsiteUrl} downloaded {data.WebsiteData.Length} characters long.\n";
+            // simulating sending to DB
+            await File.AppendAllTextAsync(filePath, text);
+        }
+        
+    }
+
+
 
     private static async Task RunDownloadAsyncParallel()
     {
         List<string> websites = PrepData();
         List<Task<WebsiteDataModel>> tasks = new List<Task<WebsiteDataModel>>();
+        StringBuilder sb = new StringBuilder();
 
         foreach (string website in websites)
         {           
@@ -113,29 +156,18 @@ class Program
 
         var results = await Task.WhenAll(tasks); // wait until all results are returned. return = A list of WebsiteDataModel
 
-        List<Task> writeToFile = new List<Task>();
-
-        foreach (var result in results)
+        // simulateing bulk insert: using a buffer to write data at once
+        foreach (var data in results)
         {
-            writeToFile.Add(Task.Run(() => ReportWebsiteInfoParallel(result)));
+            if(data is not null)
+                sb.Append($"{data.WebsiteUrl} downloaded {data.WebsiteData.Length} characters long.\n");
         }
+
+        string filePath = "resultsParallel.txt";
+
+        await File.AppendAllTextAsync(filePath, sb.ToString());
     }
 
-    private static async Task ReportWebsiteInfoAsync(WebsiteDataModel data)
-    {
-        string filePath = "simple.txt";
-        string text = $"{data.WebsiteUrl} downloaded {data.WebsiteData.Length} characters long.\n";
-
-        File.AppendAllTextAsync(filePath, text);
-    }
-
-    private static void ReportWebsiteInfoParallel(WebsiteDataModel data)
-    {
-        string filePath = "simple.txt";
-        string text = $"{data.WebsiteUrl} downloaded {data.WebsiteData.Length} characters long.\n";
-
-        File.AppendAllText(filePath, text);
-    }
 }
 public class WebsiteDataModel
 {
